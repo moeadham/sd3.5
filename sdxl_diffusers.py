a1aa74a59cd40cd1f4eef4f90795638ad4c93814
Moe Adham <moeadham@gmail.com>
2025-06-09 22:55:21 +0900

    cuda

--- sdxl_diffusers.py
+++ sdxl_diffusers.py
@@ -6,6 +6,17 @@ import torch
 
 import cv2
 from PIL import Image
+from huggingface_hub import login
+
+import os
+
+# Set cache directories
+os.environ["HF_HOME"] = "./cache"
+os.environ["HUGGINGFACE_HUB_CACHE"] = "hub"
+os.environ["TRANSFORMERS_CACHE"] = "transformers"
+os.environ["HF_DATASETS_CACHE"] = "datasets"
+# Set cache directory
+cache_dir = "./cache"
 
 prompt = "studio ghibli style"
 negative_prompt = "low quality, bad quality, sketches"
@@ -18,11 +29,15 @@ image = load_image(
 # initialize the models and pipeline
 controlnet_conditioning_scale = 0.85  # recommended for good generalization
 controlnet = ControlNetModel.from_pretrained(
-    "diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float16
+    "diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float16, cache_dir=cache_dir
 )
 vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
 pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", controlnet=controlnet, vae=vae, torch_dtype=torch.float16
+    "stabilityai/stable-diffusion-xl-base-1.0", 
+    controlnet=controlnet, 
+    vae=vae, 
+    torch_dtype=torch.float16,
+    cache_dir=cache_dir
 )
 #pipe.enable_model_cpu_offload()
 pipe.to("cuda")
